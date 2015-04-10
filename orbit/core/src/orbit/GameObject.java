92b0b3dfbfd86246a49446a09040f3c46ec5fb7c
Theodore Park <theodorehoonsikpark@gmail.com>
2015-04-10 02:38:49 -0700

    Started working on GameObject and DesktopLauncher

--- orbit/core/src/orbit/GameObject.java
+++ orbit/core/src/orbit/GameObject.java
@@ -1,5 +1,51 @@
 package orbit;
 
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
 public class GameObject {
+	private Vector2 position; //Vector for position, velocity, acceleration (like Unity)
+	private Vector2 velocity;
+	private Vector2 acceleration;
+	private Rectangle bounds; //Rectangle for checking collisions
+	private float rotation;
+	private float mass;
+	
+	public GameObject (float x, float y, float width, float height){
+		/*
+		 * WARNING: It seems that libgx, unlike most graphical libraries,
+		 * treats the screen as: x going left to right and y going down to up.
+		 * Most graphical libraries treat y as going from up to down, i.e. adding i to y will make y go down screen i pixels.
+		 * Just a heads up when messing with coordinate values.
+		 * 
+		 */
+		this.position = new Vector2(x,y);
+		this.bounds = new Rectangle(x - width/2, y - height/2, width, height); 
+		//Rectangles are initialized as Rectangle(x,y,width,height), with x and y being the coordinates of the lower left of rectangle
+		this.velocity = new Vector2();
+		this.acceleration = new Vector2();
+		rotation = 0;
+		mass = 0;
+	}
+	
+	public void setVelocity(float x, float y){
+		this.velocity = new Vector2(x,y);
+	}
+	
+	public void setAcceleration(float x, float y){
+		this.acceleration = new Vector2(x,y);
+	}
+	
+	public void setRotation(float degrees){
+		this.rotation = degrees;
+	}
+	
+	public void setMass(float mass){
+		this.mass = mass;
+	}
+	
+	public void setBounds (float x, float y, float width, float height){
+		this.bounds = new Rectangle(x - width/2, y - height/2, width, height); 
+	}
 
 }
--- orbit/desktop/src/com/orbit/game/desktop/DesktopLauncher.java
+++ orbit/desktop/src/com/orbit/game/desktop/DesktopLauncher.java
@@ -8,6 +8,9 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Interplanet Orbit";
+		config.width = 1024;
+		config.height = 600;
 		new LwjglApplication(new OrbitGame(), config);
 	}
 }
