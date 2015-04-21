807ec3a716e382a355a0fca5f75abe053c5e7f00
Steven Li <listeven@usc.edu>
2015-04-21 14:01:42 -0700

    github fucked shit up

--- orbit/desktop/src/com/orbit/game/desktop/DesktopLauncher.java
+++ orbit/desktop/src/com/orbit/game/desktop/DesktopLauncher.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.orbit.game.desktop;
 
 import orbit.Orbit;
@@ -20,26 +19,3 @@ public class DesktopLauncher {
 		new LwjglApplication(new OrbitGame(), config);
 	}
 }
-=======
-package com.orbit.game.desktop;
-
-import orbit.Orbit;
-import orbit.OrbitGame;
-
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-
-public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Interplanet Orbit";
-		config.width = 1024;
-		config.height = 600;
-
-		new LoginWindow(new Orbit()); //uncomment to see the gui windows
-		//new LobbyWindow(); 
-		//new ProfileWindow();
-		//new LwjglApplication(new OrbitGame(), config);
-	}
-}
->>>>>>> branch 'master' of https://github.com/kyle-v/orbit.git
