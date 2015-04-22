85ec598fc347ca565594680b85ff10ba387caa3e
laisteve17 <laisteve@usc.edu>
2015-04-22 00:23:27 -0700

    I HATE PRINTLINE

--- orbit/core/src/orbit/Database.java
+++ orbit/core/src/orbit/Database.java
@@ -62,7 +62,7 @@ public class Database implements Serializable{
 			return false;
 		}
 		else{
-			System.out.println("PASSWORD: " + usernameToUserMap.get(username).getPass() + ", input: " + password);
+			//System.out.println("PASSWORD: " + usernameToUserMap.get(username).getPass() + ", input: " + password);
 			if(usernameToUserMap.get(username).getPass().equals(password)){
 				return true;
 			}
