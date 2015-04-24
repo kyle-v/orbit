a8254be85795f7ed859e4cbb16f84c64d8331358
Theodore Park <theodorehoonsikpark@gmail.com>
2015-04-24 00:04:08 -0700

    fixed desktop launcher again

--- orbit/desktop/src/com/orbit/game/desktop/DesktopLauncher.java
+++ orbit/desktop/src/com/orbit/game/desktop/DesktopLauncher.java
@@ -21,27 +21,12 @@ public class DesktopLauncher {
 		//lw.setVisible(true);
 //		ProfileWindow pw = new ProfileWindow(null);
 //		pw.setVisible(true);
-<<<<<<< HEAD
-		
-=======
->>>>>>> 20856e67d40ff307fe33450cd038cb8f120a222c
 		ArrayList<User> users = new ArrayList<User>();
 		users.add(new User("Kyle","P"));
 		users.add(new User("Steven", "LJK"));
 		ArrayList<String> IPs = new ArrayList<String>();
 		IPs.add("localhost");
 		IPs.add("localhost");
-<<<<<<< HEAD
-		new LwjglApplication(new OrbitGame(users,IPs,0), config);
-//		ArrayList<User> users = new ArrayList<User>();
-//		users.add(new User("Kyle","P"));
-//		users.add(new User("Steven", "LJK"));
-//		ArrayList<String> IPs = new ArrayList<String>();
-//		IPs.add("localhost");
-//		IPs.add("localhost");
-//		new LwjglApplication(new OrbitGame(users,IPs,1), config);
-=======
 		new LwjglApplication(new OrbitGame(users,IPs,1), config);
->>>>>>> 20856e67d40ff307fe33450cd038cb8f120a222c
 	}
 }
